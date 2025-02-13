e6a3c4dfed84d8b272183122dbcc1e2154f54e27
Luis H. Hernandez <guicholh@gmail.com>
2025-02-13 00:04:19 -0600

    completed chapter 9.4

---
+++ migrations/000003_seed_movies.up.sql
@@ -0,0 +1,11 @@
+INSERT INTO movies (title, year, runtime, genres) VALUES
+    ('Black Panther', 2018, 134, '{"action", "adventure", "sci-fi"}'),
+    ('The Dark Knight', 2008, 152, '{"action", "crime", "drama"}'),
+    ('Pulp Fiction', 1994, 154, '{"crime", "drama"}'),
+    ('The Shawshank Redemption', 1994, 142, '{"drama"}'),
+    ('Inception', 2010, 148, '{"action", "adventure", "sci-fi"}'),
+    ('Parasite', 2019, 132, '{"drama", "thriller"}'),
+    ('Spider-Man: Into the Spider-Verse', 2018, 117, '{"animation", "action", "adventure"}'),
+    ('The Godfather', 1972, 175, '{"crime", "drama"}'),
+    ('Everything Everywhere All at Once', 2022, 139, '{"action", "adventure", "comedy"}'),
+    ('Coco', 2017, 105, '{"animation", "adventure", "family"}');
